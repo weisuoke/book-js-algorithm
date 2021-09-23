b930c909fb65c2a98e8760db99f9d8c3079729e0
梅林的法杖 <530264127@qq.com>
2021-09-23 22:39:01 +0800

    feat: leetcode 9

---
+++ src/leetcode/math/math9.ts
@@ -0,0 +1,15 @@
+const isPalindrome = (x: number): boolean => {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
+
+  let revertedNumber: number = 0;
+  while (x > revertedNumber) {
+    revertedNumber = revertedNumber * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return x === revertedNumber || x === Math.floor(revertedNumber / 10);
+};
+
+export default isPalindrome;
